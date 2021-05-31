c98c975c773d2283df5ba92f32598aa287b7cc7d
ondrikk <fajnygo@yahoo.pl>
2021-05-31 18:40:42 +0200

    Closes #14, fixes #17

--- imap_user_auth/app/models/auth_source_imap.rb
+++ imap_user_auth/app/models/auth_source_imap.rb
@@ -23,34 +23,40 @@ require 'timeout'
 # 4. Restart your web server
 #
 
+# Configuration values are assumed to be in database and are read as for example: self.host
+# Alternative option is reading them from plugin settings for example: Setting.plugin_imap_user_auth['host']
+
+# When logging with email username@address.com, user inputs: username
+
 class AuthSourceIMAP < AuthSource
-    def onthefly_register
-        true
-    end
-    
     def authenticate(login, password)
         puts "[DEBUG] 5.1"
         # Define your IMAP server
-        self.host = "poczta.interia.pl"
+        #self.host = "poczta.interia.pl"
         #self.host = "127.0.0.1" 
         # self.port = 143
-        self.port = 993
+        #self.port = 993
+
+        suffix = self.attr_mail
+        login += suffix
+
         retVal = {
             :firstname => self.attr_firstname,
             :lastname => self.attr_lastname,
-            :mail => self.attr_mail,
+            :mail => login,
             :auth_source_id => self.id
         }
         # Email as account if you use Google Apps
         #suffix = "@mail.local";
-        suffix = "@interia.pl"
-        if not login.end_with?(suffix)
-            login += suffix
-        end
+        #suffix = "@interia.pl"
+        #if not login.end_with?(suffix)
+        #    login += suffix
+        #end
+
         # Authenticate
         options = {
                 :port => self.port,
-                :ssl => true
+                :ssl => self.tls ? true : {:verify_mode => OpenSSL::SSL::VERIFY_NONE}
                 #  {
                 #   #add this to bypass OpenSSL::SSL::SSLError 
                 #   #(hostname does not match the server certificate) error.
