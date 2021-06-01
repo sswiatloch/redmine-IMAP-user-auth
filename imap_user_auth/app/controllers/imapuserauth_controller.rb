2f5c5a45e306aeb9e499eac38502c352718f7149
Stanisław Światłoch <sswiatloch@hotmail.com>
2021-06-01 18:10:39 +0200

    Working auth_source configuration view, closes #9, closes #12

--- imap_user_auth/app/controllers/imapuserauth_controller.rb
+++ imap_user_auth/app/controllers/imapuserauth_controller.rb
@@ -6,6 +6,18 @@ class ImapuserauthController < ApplicationController
 
   # TODO
   def updateDatabase
-    AuthSourceIMAP.create(name: Setting.plugin_imap_user_auth['name'], onthefly_register: Setting.plugin_imap_user_auth['otfr'])
+    setting = params[:settings] ? params[:settings].permit!.to_h : {}
+    Setting.send "plugin_imap_user_auth=", setting
+
+    imap = AuthSource.where(type: 'AuthSourceIMAP').first_or_initialize
+    imap.name = setting['name']
+    imap.host = setting['host']
+    imap.port = setting['port']
+    imap.attr_mail = setting['suffix']
+    imap.onthefly_register = setting['otfr'] ? true : false
+    imap.tls = setting['ssl'] ? true : false
+    imap.save
+
+    flash[:notice] = l(:notice_successful_update)
   end
 end
--- imap_user_auth/config/routes.rb
+++ imap_user_auth/config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'imapuserauth', to: 'imapuserauth#index'
-# TODO add route to update database (post)
+post 'imapuserauth/update', to: 'imapuserauth#updateDatabase'
