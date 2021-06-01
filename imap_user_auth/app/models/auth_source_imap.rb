2e9c98130147953a5076b6d1ecb80ed393ad6b96
ondrikk <fajnygo@yahoo.pl>
2021-06-01 23:27:40 +0200

    Removed unused files and not relevant comments

--- imap_user_auth/app/models/auth_source_imap.rb
+++ imap_user_auth/app/models/auth_source_imap.rb
@@ -13,16 +13,6 @@
 require "net/imap" 
 require 'timeout'
 
-#
-# HOWTO
-#
-# 1. Execute the SQL
-#    INSERT INTO auth_sources (type, name) values ("AuthSourceIMAP", "IMAP")
-# 2. Change as you like
-# 3. Redmine: set the user authentication mode to IMAP
-# 4. Restart your web server
-#
-
 # Configuration values are assumed to be in database and are read as for example: self.host
 # Alternative option is reading them from plugin settings for example: Setting.plugin_imap_user_auth['host']
 
@@ -31,11 +21,6 @@ require 'timeout'
 class AuthSourceIMAP < AuthSource
     def authenticate(login, password)
         puts "[DEBUG] 5.1"
-        # Define your IMAP server
-        #self.host = "poczta.interia.pl"
-        #self.host = "127.0.0.1" 
-        # self.port = 143
-        #self.port = 993
 
         suffix = self.attr_mail
         login += suffix
@@ -46,27 +31,15 @@ class AuthSourceIMAP < AuthSource
             :mail => login,
             :auth_source_id => self.id
         }
-        # Email as account if you use Google Apps
-        #suffix = "@mail.local";
-        #suffix = "@interia.pl"
-        #if not login.end_with?(suffix)
-        #    login += suffix
-        #end
 
-        # Authenticate
         options = {
                 :port => self.port,
                 :ssl => self.tls ? (Setting.plugin_imap_user_auth['bypass'] ? {:verify_mode => OpenSSL::SSL::VERIFY_NONE} : true ) : false
-                #  {
-                #   #add this to bypass OpenSSL::SSL::SSLError 
-                #   #(hostname does not match the server certificate) error.
-                #   :verify_mode => OpenSSL::SSL::VERIFY_NONE
-                # }
         }
+		
         begin
             puts "[DEBUG] 5.2"
             imap = Net::IMAP.new(self.host, options)
-            #substituted imap.authenticate with imap.login
             imap.login(login, password)
         rescue Net::IMAP::NoResponseError => e
             puts "[DEBUG] 5.3"
